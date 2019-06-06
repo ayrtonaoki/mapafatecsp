6330dc730baa3e383a67d7d5ed7b8363262a786e
ayrtonaoki <ayrtonsasouza@gmail.com>
2019-06-06 15:18:55 -0300

    Add script for change map

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-function selecionaLocal(local) {
-	let totalPredios = document.getElementsByTagName('rect');
-	for (i = 0; i < totalPredios.length; i++) {
-		totalPredios.item(i).style.fill = '#545B62';
+function selecionaLocal(local, andar) {
+	if (andar) {
+		let totalPredios = document.getElementById(andar).contentWindow.document.getElementsByTagName('rect');
+		for (i = 0; i < totalPredios.length; i++) {
+			totalPredios.item(i).style.fill = '#545B62';
+		};
+		document.getElementById(andar).contentWindow.document.getElementById(local).style.fill = '#007bff';
+	}
+	else {
+		let totalPredios = document.getElementsByTagName('rect');
+		for (i = 0; i < totalPredios.length; i++) {
+			totalPredios.item(i).style.fill = '#545B62';
+		};
+
+		document.getElementById(local).style.fill = '#007bff';
+	}
+};
+
+function mudaMapa(id) {
+	let andar = document.getElementById(id);
+	let totalMapas = document.getElementsByTagName('iframe');
+
+	for (i = 0; i < totalMapas.length; i++) {
+		totalMapas.item(i).style.display = 'none';
 	};
 
-	document.getElementById(local).style.fill = '#007bff';
+	andar.style.display = 'inline';
 }
